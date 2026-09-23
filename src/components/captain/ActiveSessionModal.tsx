533763cad6d1485d4d424034e8ef506a62095d00
scrrana2000-sys <scrrana2000@gmail.com>
2026-09-23 10:54:16 +0530

    Fix missing useEffect import in captain cancellation UI

--- src/components/captain/ActiveSessionModal.tsx
+++ src/components/captain/ActiveSessionModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table, TableSession } from '../../types/table';
 import { Order } from '../../types/order';
 import { KOT } from '../../types/kot';
